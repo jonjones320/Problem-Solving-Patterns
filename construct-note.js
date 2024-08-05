513639091c5214fa315aff345fbe1c3de16c23cc
JJ <jonjones320@gmail.com>
2024-08-04 22:41:59 -0700

    Initial commit.

---
+++ construct-note.js
@@ -0,0 +1,16 @@
+// add whatever parameters you deem necessary
+function constructNote(msg, letters) {
+    letterCount = {};
+
+    for (let letter of letters) {
+        letterCount[letter] = (letterCount[letter] || 0) + 1;
+    };
+
+    for (let letter of message) {
+        if (!letterCount[letter] || letterCount[char] === 0) {
+            return false;
+        }
+        letterCount[char]--;
+    }
+    return true;
+}
